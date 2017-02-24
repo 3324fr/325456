6edea2cfe7ff4f98ff6aa7b061618fc7ba7492f0
sudoyum21 <ba.samson.lam@gmail.com>
2017-02-23 23:06:19 -0500

    adding tv and etext

--- app/src/main/java/inf8405_tp2/tp2/MainActivity.java
+++ app/src/main/java/inf8405_tp2/tp2/MainActivity.java
@@ -2,18 +2,31 @@ package inf8405_tp2.tp2;
 
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private String m_TemporateUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextPrice = (EditText) findViewById(R.id.et_username);
+        editTextPrice.addTextChangedListener(new GenericTextWatcher(editTextPrice));
     }
 
     public void picture(View view) {
@@ -22,4 +35,35 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void OnClickConfirm(View view){
+        m_TemporateUserName = ((EditText)findViewById(R.id.et_username)).getText().toString();
+        Toast.makeText(this, m_TemporateUserName, Toast.LENGTH_SHORT).show();
+    }
+
+    public void OnClickGroup(View view) {
+    }
+
+
+
+
+    private class GenericTextWatcher implements TextWatcher {
+
+        private View view;
+        private GenericTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+        {
+            switch(view.getId()){
+                case R.id.et_username:
+                    break;
+            }
+        }
+
+        public void afterTextChanged(Editable editable)
+        {
+        }
+    }
 }
